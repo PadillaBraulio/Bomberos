a212ccdef7326c743f61da94a1e2c1fbadce582e
Braulio <brauliojuancarlos@gmail.com>
2016-07-17 12:45:29 -0600

    Create UI for mainactivity and SharedData class to save data

---
+++ Bomberos/app/src/main/java/project/bomberos/SharedData.java
@@ -0,0 +1,28 @@
+package project.bomberos;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by root on 16/07/16.
+ */
+public class SharedData {
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor  sharedEditor;
+    private Context context;
+
+    public SharedData(SharedPreferences sharedPrf, Context context) {
+        this.sharedPref = sharedPrf;
+        this.sharedEditor = sharedPrf.edit();
+        this.context = context;
+    }
+
+    protected void putAmbulanceNumber(String AmbulanceNumber){
+        sharedEditor.putString(context.getString(R.string.AmbulanceNUmber), AmbulanceNumber);
+        sharedEditor.commit();
+    }
+    protected String getAmbulanceNumber(){
+        String AmbulanceNumber = context.getString(R.string.AmbulanceNUmber);
+        return sharedPref.getString(AmbulanceNumber, null);
+    }
+}
